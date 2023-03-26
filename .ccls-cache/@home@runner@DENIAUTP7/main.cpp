3f87fb3f8803093721215c9f668dc0185becb8b7
mdeniau2022 <124585738+RIVEIDD@users.noreply.github.com>
2023-03-26 13:23:58 +0000

    test 4

--- .ccls-cache/@home@runner@DENIAUTP7/main.cpp
+++ .ccls-cache/@home@runner@DENIAUTP7/main.cpp
@@ -6,26 +6,7 @@ using namespace std;
 
 int main(){
   point p; 
-  point y(5,5); 
-  point b = y; 
-  // cout << "les valeurs du point sont :" << p.getx() << " en abscisse et " << p.gety() << " en ordonné." << endl;  
-  // p.translater(5,5);
-  // cout << "les valeurs du point sont :" << p.getx() << " en abscisse et " << p.gety() << " en ordonné." << endl;  
-  // p.translaterp(y);
-  // cout << "les valeurs du point sont :" << p.getx() << " en abscisse et " << p.gety() << " en ordonné." << endl; 
-  cout << "les valeurs du point sont :" << b.getx() << " en abscisse et " << b.gety() << " en ordonné." << endl; 
-  cout << b<<endl; 
-  // cout << y<< endl; 
-  // y += 5;
-  // cout << y << endl; 
-  forme f; 
-  f.perimetre (5.6);
-  f.surface(6.5); 
-  f.getc();
-  cout << f.getp() << endl;
-  cout << f.gets() << endl;
-  f += 1; 
-  f.getc();
-  cout << f<<endl; 
-  qse<f
+  cout << p<<endl; 
+  p+=5; 
+  cout <<p; 
 }
